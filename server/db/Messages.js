d21eb1b8e4a3cb8fe4d53120f61388b41afbbcb6
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-15 01:06:22 +0530

    Created Schema and get, create methods ➕

---
+++ server/db/Messages.js
@@ -0,0 +1,35 @@
+const { func } = require('joi')
+const Joi = require('joi')
+const db = require('./connection')
+
+const schema = Joi.object().keys({
+    username: Joi.string().alphanum().required(),
+    subject: Joi.string().required(),
+    message: Joi.string().max(500).required(),
+    imageURL: Joi.string().uri({
+        scheme: [
+            /https?/
+        ]
+    })
+})
+
+const messages = db.get('messages')
+
+const getAll = () => messages.find()
+
+const create = (message) => {
+    const result = schema.validate(message)
+    if(result.error == null) {
+        message.created = new Date()
+        console.log(message)
+        return messages.insert(message)
+    } else {
+        console.log('rejected da maamu')
+        return Promise.reject(result.error)
+    }
+}
+
+module.exports = {
+    getAll,
+    create
+}
