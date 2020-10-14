e8f49e736e0f0bad43ed3e988c8d670e0b9b82f3
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-15 01:12:32 +0530

    Created GET, POST endpoints for messages

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
+require('dotenv').config()
+
+const messages = require('./db/Messages')
+
 const app = express()
 
 app.use(morgan('tiny'))
@@ -13,6 +17,17 @@ app.get('/', (req, res)=> {
     res.json({
         msg: 'Full stack message board 🎉'
     })
+})
+
+app.get('/messages', (req, res) => {
+    messages.getAll().then(messages => res.json(messages))
+})
+
+app.post('/messages', (req, res) => {
+    console.log(req.body)
+    messages.create(req.body)
+        .then(message => res.json(message))
+        .catch(err => res.status(500).json(err))
 })
 
 const port = process.env.PORT || 3000
